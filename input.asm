b85e930e3fbf649c13435541f739aad3e13fbd64
Wiebo de Wit <wiebo.de.wit@gmail.com>
2015-12-01 20:44:43 +0100

    Changed control code and game loop order to support new input method

--- input.asm
+++ input.asm
@@ -1,7 +1,9 @@
 
-.const KEYPRESSED = $cb 	// scnkey puts code of held key here.
+.const keyPressed = $cb 	// scnkey puts code of held key here.
+.const INPUTDELAY = 15	 	// update delay between input checks
 
-// keycodes
+// keycodes to check for in inputResult values
+// valid controls are also used for joystick results.
 
 .const LEFT = 47 			// , <
 .const RIGHT = 44	 		// . >
@@ -10,99 +12,62 @@
 .const DOWN = 1 			// ENTER
 .const PAUSE = 41 			// P
 .const RESET = 4	 		// F1
+.const CHANGEBACKGROUND = 0 // F3
+.const CHANGECOLOUR = 0 	// F5
 .const NOKEY = 64 			// hehe
+.const NOINPUT = 253		// no input detected
 
-.const INPUTDELAY = 7	 	// update delay between input checks
+// this byte holds the result of the input query
+// game modes can check this byte and get the
+// registered input
 
+inputResult:
+			.byte 0
 
-// this subroutine gets keyboard input during the game.
-// it also moves and prints the affected block
+// ------------------------------------------------------
+
+// this subroutine gets keyboard input
 // only one key at a time is registered
-// collision detection etc is also included when move is made.
-
-// reset and pause keys are checked first. if game is paused, then
-// no more keys are checked to prevent input
+// "inputResult" will hold the registered input
 
 GetKeyInput:
-			lda KEYPRESSED 			// get held key code
+			lda #NOINPUT 			// first assume there is no input
+			sta inputResult
+
+			lda keyPressed 			// get held key code
 			cmp previousKey 		// is it a different key than before?
 			bne !skip+	 			// if yes, then skip the current input delay
 									// because we want snappy controls
 doDelay:
 			dec inputDelayCounter 	// count down
-			beq !skip+
-			rts
+			beq !skip+ 				// continue if delay passed
+			rts 					// delay ongoing. exit. no input.
 !skip:
-			ldx #INPUTDELAY 		// get the delay value
-			stx inputDelayCounter 	// and restore it
-
-			sta previousKey 		// save the held key
-			cmp #NOKEY 				// none?
-			bne !nextkey+
-			rts
-!nextkey:
-			cmp #RESET 				// reset game?
-			bne !nextkey+
-			nop 					// add later
-!nextkey:
-			cmp #PAUSE
-			bne !nextkey+
-
-			lda pauseFlag 			// get the current pause flag
-			eor #%00000001 			// flip between 0 and 1
-			sta pauseFlag 			// store it
-			beq !skip+ 				// skip next bit if not paused
-
-			// game is paused. so clear the screen
-
-			lda #$01 				// set the erase flag
-			sta playAreaErase 		// so area gets cleared as well
-			jsr SavePlayArea 		// save and clear the play area
-			jmp PrintPaused
+			sta previousKey 		// save key code for next update
+			cmp #NOKEY 				// is it the no key held code?
+			bne !skip+ 				// no
+			lda #NOINPUT 			// yes. select that input result
 !skip:
-			jmp RestorePlayArea 	// restore the screen
+			sta inputResult 		// store input result
 
-!nextkey:
-			ldx pauseFlag 			// get the pause flag
-			beq !skip+ 				// continue if not in pause mode
-
-			// game is in pause mode. we do not check more keys
-			rts
-!skip:
-			cmp #LEFT 				// left key held?
-			bne !nextkey+ 			// if not check next key
-			jmp BlockLeft
-!nextkey:
-  			cmp #RIGHT
-  			bne !nextkey+
-  			jmp BlockRight
-!nextkey:
-  			cmp #TURNCOUNTER 		// turn counter-clockwise held?
-  			bne !nextkey+ 			// no
-  			jmp BlockRotateCCW
-!nextkey:
- 			cmp #TURNCLOCK
- 			bne !nextkey+
- 			jmp BlockRotateCW
-!nextkey:
-			cmp #DOWN
-			bne !nextkey+
-			jmp BlockDown
-!nextkey:
+			lda #INPUTDELAY 		// restore key delay counter
+			sta inputDelayCounter
 			rts
 
 // -------------------------------------------------
 
 // this subroutine checks for joystick input from port 2
 // the input register is rotated and the carry bit is checked
-// we have only one joystick button, so UP is used for rotate CCW :/
-
+// we have only one joystick button, so UP is used for rotate CCW
+// "inputResult" will hold the registered input
 
 .const CIAPRA = $dc00 				// joystick port 2 input register
 .const NOJOY  = $ff 				// value for no joy input
 
-
 GetJoyInput:
+			lda #NOINPUT 			// assume there is no input
+			sta inputResult
+
 			lda CIAPRA 				// load the input byte
 			cmp previousJoy 		// same as previous input?
 			bne !skip+ 				// no, so skip delay
@@ -111,125 +76,60 @@ joyDelay:
 			beq !skip+ 				// continue if delay complete
 			rts
 !skip:
-			ldx #INPUTDELAY 		// reset the counter
+			ldx #INPUTDELAY 		// reset the delay counter
 			stx inputDelayCounter
 
 			sta previousJoy 		// save this input value
-			cmp #NOJOY 				// is there input?
-			bne !nextjoy+ 			// yes, continue
-			rts 					// no. done.
+			cmp #NOJOY 				// same as noinput?
+			bne !nextjoy+ 			// no, so go check the possiblities
+
+			lda #NOINPUT 			// there is no input, store it
+			sta inputResult 		// in result
+			rts
 !nextjoy:
 			clc 					// clear the carry bit
 			lsr 					// check bit 0: joy up
 			bcs !nextjoy+
-			jmp BlockRotateCCW 		// we only have one button so
-									// compromise with using up
+
+			lda #TURNCOUNTER 		// store the correct code ...
+			sta inputResult 		// as result
+			rts
 !nextjoy:
 			lsr 					// check bit 1: joy down
 			bcs !nextjoy+ 			// bit set means not pressed
-			jmp BlockDown
+			lda #DOWN
+			sta inputResult
+			rts
 !nextjoy:
 			lsr 					// check bit 2: joy left
 			bcs !nextjoy+
-			jmp BlockLeft
+			lda #LEFT
+			sta inputResult
+			rts
 !nextjoy:
 			lsr 					// check bit 3: joy right
 			bcs !nextjoy+
-			jmp BlockRight
+			lda #RIGHT
+			sta inputResult
+			rts
 !nextjoy:
 			lsr 					// check bit 4: joy fire button
 			bcs !nextjoy+
-			jmp BlockRotateCW
+			lda #TURNCLOCK
+			sta inputResult
+			rts
 !nextjoy:
 			rts 					// those were all the relevant bits.
-
-// -------------------------------------------------
-// movement subroutines
-
-BlockLeft:
-			jsr EraseBlock 			// remove block on this position
-			dec blockXposition 		// alter block position
-			jsr CheckBlockSpace 	// will it fit?
-			beq !skip+ 				// yes. print it
-			inc blockXposition 		// no. move it back
-!skip:
-			jsr PrintBlock
- 			rts
-
-BlockRight:
-  			jsr EraseBlock
-  			inc blockXposition
-  			jsr CheckBlockSpace
- 			beq !skip+
-  			dec blockXposition
-!skip:
-  			jsr PrintBlock
-  			rts
-
-BlockDown:
-			jsr EraseBlock
-			inc blockYposition
-			jsr CheckBlockSpace
-			beq !skip+
-
-			// block doesn't fit
-			dec blockYposition
-			jsr PrintBlock
-
-			lda #$04 				// we made block drop
-			sta fallDelayTimer 		// so create new one without delay
-			rts
-!skip:
-			jsr PrintBlock
-
- 			lda #$04 	 			// have a smaller falldelay
- 			sta fallDelayTimer 		// as we move down ourselves
-
- 			// moving the block down gives points
-
-			lda #1
- 			sta addition
- 			lda #0
- 			sta addition+1
- 			sta addition+2
- 			jsr AddScore
- 			jsr PrintScore
-			rts
-
-BlockRotateCCW:
-  			jsr EraseBlock 			// remove block on this position
-  			lda #$01 				// yes. 1 means counter clock wise
- 			jsr AnimateBlock 		// rotate it
- 			jsr CheckBlockSpace 	// will it fit?
- 			beq !skip+ 				// yes, print it
- 			lda #$00 				// no
-  			jsr AnimateBlock 		// turn it back
-!skip:
-  			jsr PrintBlock
-  			rts
-
-BlockRotateCW:
- 			jsr EraseBlock
- 			lda #$00
- 			jsr AnimateBlock
- 			jsr CheckBlockSpace
- 			beq !skip+
- 			lda #$01
- 			jsr AnimateBlock
-!skip:
- 			jsr PrintBlock
- 			rts
+									// if we get to this, NOINPUT is still
+									// stored in inputResult.
 
 // ------------------------------------------------
-
-pauseFlag:
-			.byte 0 				// game is pause when this is set to 1
 
 inputDelayCounter:
 			.byte INPUTDELAY		// if this reaches 0, the player input is read
 
 previousKey:
-			.byte 64				// previous key held
+			.byte DOWN				// previous key held
 
 previousJoy:
 			.byte 255 				// previous joy direction held
--- play.asm
+++ play.asm
@@ -66,7 +66,7 @@ exitflash:
 			// flashing is all done
 
 			jsr AddLinesTotal 		// add the made lines to total
-			jsr PrintTotalLinesMade // and print these.
+			jsr PrintTotalLinesMade // and print these
 
 			jsr AddLineScore		// add score made by lines
 			jsr PrintScore 			// show the score
@@ -85,52 +85,98 @@ exitflash:
 
 			lda levelLinesCounter	// get lines made so far at this level
 			cmp #linesPerLevel 		// did we make enough to go up a level?
-			bcc nolevelinc 			// no: If the C flag is 0, then A (unsigned) < NUM (unsigned)
+			bcc !skip+ 				// no: If the C flag is 0, then A (unsigned) < NUM (unsigned)
 									// and BCC will branch
-
 			jsr AddLevel 			// go up 1 level
+!skip:
+			// add a new block to play with
 
-nolevelinc:
-			// blocks are moved down, score is added
-			// now add a new block to play with
-
-			jsr NewBlock 		// create a new block
-			rts
+			jsr NewBlock 			// create a new block
 
 continueplay:
 
-			jsr GetKeyInput 	// get player input
+			// do game logic
 
-			// 'p' might have been pressed, so check pause flag
-
-			lda pauseFlag 		// are we in pause mode?
-			bne endplay 		// if yes then do no more
-
-			// we're not in pause mode, so continue
-
-			jsr GetJoyInput 	// get joystick input
-
-			jsr DropBlock 		// move the block down if delay has passed
-			cmp #$02 			// Acc=2 means that a new block is needed
-			bne endplay 		// no. end the loop
+			jsr DropBlock 			// move the block down if delay has passed
+			cmp #$02 				// Acc=2 means that a new block is needed
+			bne !skip+
 
 			// a new block is needed so we might have made line(s)
 
-			jsr CheckLines 		// check if line(s) has been made.
-			lda linesMade 		// get lines made value
-			bne endplay 		// more than 0! don't create a new block just yet, as the next
-								// loop will flash the lines and THEN create a new block
+			jsr CheckLines 			// check if line(s) has been made.
+			lda linesMade 			// get lines made value
+			beq nolines				// no lines made, so continue game
 
+			rts 					// do not create a new block just yet.
+									// loop will flash the lines and THEN create a new block
+
+nolines:
 			// we made no lines. so continue
 
-			jsr NewBlock 		// select a new block
-			beq endplay			// Acc=0 means the new block fits, so continue
+			jsr NewBlock 			// select a new block
+			beq !skip+				// Acc=0 means the new block fits, so continue
 
-			// block didn't fit, so game over!
+			// new block didn't fit, so game over!
 
 			jmp EndPlayMode
-endplay:
+!skip:
+
+			jsr GetKeyInput 		// get player input from keyboard
+			ldx inputResult 		// what was the input?
+
+			// first check generic keys
+
+			cpx #RESET
+			bne !skip+
+
+			// reset the game
+			brk
 			rts
+!skip:
+			cpx #PAUSE
+			bne !skip+
+
+			jsr TogglePause
+
+			lda pauseFlag 			// get pause flag
+			beq !skip+ 				// continue if not paused
+			rts 					// paused, we're done
+!skip:
+			cpx #NOINPUT 			// other input?
+			bne !skip+				// yes. process it
+
+			// no other input from keys, check joystick
+
+			jsr GetJoyInput
+			ldx inputResult
+			cpx #NOINPUT 			// valid input?
+			bne !skip+				// yes, process it
+			rts 					// no input from joy either. end!
+!skip:
+			// only block control movement input left, so check those
+
+			cpx #LEFT
+			bne !skip+
+			jmp BlockLeft
+!skip:
+  			cpx #RIGHT
+  			bne !skip+
+  			jmp BlockRight
+!skip:
+  			cpx #TURNCOUNTER
+  			bne !skip+
+  			jmp BlockRotateCCW
+!skip:
+ 			cpx #TURNCLOCK
+ 			bne !skip+
+ 			jmp BlockRotateCW
+!skip:
+			cpx #DOWN
+			bne !skip+
+			jmp BlockDown
+!skip:
+			rts
+
 
 
 // ------------------------------------------------
@@ -183,6 +229,24 @@ AddLevel:
 			sta fallDelayTimer 		// reset the current delay counter
 			rts
 
+
+// --------------------------------------------------
+
+TogglePause:
+			lda pauseFlag 			// get the current pause flag
+			eor #%00000001 			// flip between 0 and 1
+			sta pauseFlag 			// store it
+
+			cmp #$01 				// pause mode?
+			beq !skip+ 				// yes
+			jmp RestorePlayArea 	// no, restore the screen
+!skip:
+			// game is paused. so clear the screen
+
+			lda #$01 				// set the erase flag
+			sta playAreaErase 		// so area gets cleared as well
+			jsr SavePlayArea 		// save and clear the play area
+			jmp PrintPaused
 
 // --------------------------------------------------
 
