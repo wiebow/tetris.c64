ffb0c317f7b159ec384f3c510fd8b8b87a5f7687
Wiebo de Wit <wiebo.de.wit@gmail.com>
2017-01-21 19:04:37 +0100

    made simpler screen data print routines

--- screens.asm
+++ screens.asm
@@ -1,39 +1,192 @@
-
-
 // code concerning game screens
 
-// game screen dimensions
-.const screenWidth = 21
-.const screenHeight = 21
+// well dimensions
+.const wellWidth = 10
+.const wellHeight = 20
 
+// screen dimensions
+.const screenWidth = 21 		// columns
+.const screenHeight = 21		// rows
+
+// screen id
+.const SCREEN_TITLE = 0
+.const SCREEN_CREDITS = 1
+.const SCREEN_KEYS = 2
+.const SCREEN_LEVELSELECT = 3
+.const SCREEN_PLAY = 4
+
+// well data id
+.const WELL_PAUSE = 0
+.const WELL_GAMEOVER = 1
+.const WELL_TEMP = 2
+
+// -----------------------------------------
+
+// set Y register with the index of the screen you want to print
+PRINT_SCREEN:
+	// get source address
+	lda screenDataLow,y
+	sta readScreen+1
+	lda screenDataHigh,y
+	sta readScreen+2
+	// reset screen address
+	lda #10
+	sta writeScreen+1
+	lda #04
+	sta writeScreen+2
+	// set the data counters
+	lda #screenWidth
+	sta dataWidth
+	lda #screenHeight
+	sta dataHeight
+
+	ldx #0 					// reset read index
+	ldy #0 					// reset write index
+readScreen:
+	lda $ffff,x 			// get data
+writeScreen:
+	sta $ffff,y 			// store at destination
+	inx 					// update read counter
+	bne !skip+ 				// roll over?
+	inc readScreen+2  		// yes. go to next memory page
+!skip:
+	iny 					// update row counter
+	cpy dataWidth			// this row done?
+	bne readScreen 	 			// no, continue
+	ldy #$00 				// reset the row counter
+	lda writeScreen+1 			// get lo byte of current screen position
+	clc
+	adc #40 				// add 40 to that, goto next row
+	bcc !skip+ 				// overflow?
+	inc writeScreen+2  			// then go to next memory page
+!skip:
+	sta writeScreen+1 			// store lo byte
+
+	dec dataHeight 			// update counter
+	bne readScreen 				// not all rows done yet
+	rts
+
+// -----------------------------------------
+
+// set Y register with the index of the well text you want to print
+PRINT_WELLDATA:
+	// get source address
+	lda wellDataLow,y
+	sta wellRead+1
+	lda wellDataHigh,y
+	sta wellRead+2
+	// reset screen address
+	lda #12
+	sta wellWrite+1
+	lda #04
+	sta wellWrite+2
+	// set the data counters
+	lda #wellWidth
+	sta dataWidth
+	lda #wellHeight
+	sta dataHeight
+
+	ldx #0 					// reset read index
+	ldy #0 					// reset write index
+wellRead:
+	lda $ffff,x 			// get data
+wellWrite:
+	sta $ffff,y 			// store at destination
+
+	inx 					// update read counter
+	bne !skip+ 				// roll over?
+	inc wellRead+2  		// yes. go to next memory page
+!skip:
+	iny 					// update write counter
+	cpy dataWidth			// this row done?
+	bne wellRead 	 		// no, continue
+	ldy #$00 				// reset the row counter
+	lda wellWrite+1 		// get lo byte of current screen position
+	clc
+	adc #40 				// add 40 to that, goto next row
+	bcc !skip+ 				// overflow?
+	inc wellWrite+2 		// then go to next memory page
+!skip:
+	sta wellWrite+1 		// store lo byte
+
+	dec dataHeight 			// update counter
+//	lda dataHeight
+	bne wellRead 			// not all rows done yet
+	rts
+
+
+// -----------------------------------------
+
+// WriteScreenData:
+// 	// get the source address
+// 	lda dataSourceLo
+// 	sta read2+1
+// 	lda dataSourceHi
+// 	sta read2+2
+
+// 	// get the destination address
+
+// 	lda dataDestinationLo
+// 	sta write2+1
+// 	lda dataDestinationHi
+// 	sta write2+2
+
+// 	ldx #$00 				// reset read index
+// 	ldy #$00 				// reset write index
+
+// 	// start copy
+// read2:
+// 	lda $1000,x 			// get data
+// write2:
+// 	sta $1000,y 			// store at destination
+// 	inx 					// update read counter
+// 	bne !skip+ 				// roll over?
+// 	inc read2+2  			// yes. go to next memory page
+// !skip:
+// 	iny 					// update row counter
+// 	cpy dataWidth			// this row done?
+// 	bne read2 	 			// no, continue
+// 	ldy #$00 				// reset the row counter
+// 	lda write2+1 			// get lo byte of current screen position
+// 	clc
+// 	adc #40 				// add 40 to that, goto next row
+// 	bcc !skip+ 				// overflow?
+// 	inc write2+2  			// then go to next memory page
+// !skip:
+// 	sta write2+1 			// store lo byte
+
+// 	dec dataHeight 			// update counter
+// 	lda dataHeight
+// 	bne read2 				// not all rows done yet
+// 	rts
 
 // ----------------------------------------------
 
 // subroutine to clear the screen and color ram
 // also detroys sprite pointers.
 ClearScreen:
-			lda #11 			// dark grey
-			sta $d020 			// set border color
-			sta $d021 			// set screen color
-			ldx #$00 			// reset offset register
+	lda #11 			// dark grey
+	sta $d020 			// set border color
+	sta $d021 			// set screen color
+	ldx #$00 			// reset offset register
 !loop:
-			lda #$20			// #$20 is space
-			sta $0400,x 		// store in screen ram
-			sta $0500,x
-			sta $0600,x
-			sta $0700,x
-			lda #13				// light green (screen color code)
-			sta $d800,x 		// store in color ram
-			sta $d900,x
-			sta $da00,x
-			sta $db00,x
-			inx 				// increment counter
-			bne !loop- 			// continue?
+	lda #$20			// #$20 is space
+	sta $0400,x 		// store in screen ram
+	sta $0500,x
+	sta $0600,x
+	sta $0700,x
+	lda #13				// light green (screen color code)
+	sta $d800,x 		// store in color ram
+	sta $d900,x
+	sta $da00,x
+	sta $db00,x
+	inx 				// increment counter
+	bne !loop- 			// continue?
 
-	        lda #153 			// print everything in light green ...
-	        jsr $ffd2 			// from now on (ascii code)
+    lda #153 			// print everything in light green ...
+    jsr PRINT 			// from now on (ascii code)
 
-			rts
+	rts
 
 // ---------------------------------------------
 
@@ -42,124 +195,90 @@ ClearScreen:
 // If playAreaErase is set to 1 then the area is cleared as well
 
 SavePlayArea:
-			// point to the beginning of the first row
+	// point to the beginning of the first row
 
-			ldx #12 				// x position is set to 12
-			ldy #0 					// y position to 0
-			jsr SetScreenPointer 	// set screen memory pointer
+	ldx #12 				// x position is set to 12
+	ldy #0 					// y position to 0
+	jsr SetScreenPointer 	// set screen memory pointer
 
-			ldx #$00				// reset buffer pointer
-			stx currentRow 			// reset the row counter
+	ldx #$00				// reset buffer pointer
+	stx currentRow 			// reset the row counter
 !loop:
-			lda (screenPointer),y 	// get screen data
-			sta playAreaBuffer,x 	// store it in the buffer
-			lda playAreaErase 		// erase it as well?
-			beq !skip+ 				// no
-			lda #$20 				// write a space
-			sta (screenPointer),y
+	lda (screenPointer),y 	// get screen data
+	sta playAreaBuffer,x 	// store it in the buffer
+	lda playAreaErase 		// erase it as well?
+	beq !skip+ 				// no
+	lda #$20 				// write a space
+	sta (screenPointer),y
 !skip:
-			inx 					// update buffer pointer
-			iny  					// update row character counter
-			cpy #10 				// stored whole line?
-			bne !loop- 				// no, keep reading
+	inx 					// update buffer pointer
+	iny  					// update row character counter
+	cpy #10 				// stored whole line?
+	bne !loop- 				// no, keep reading
 
-			inc currentRow 			// go to the next row
-			lda currentRow 			// what value is it now?
-			cmp #20 				// all 20 rows checked?
-			beq !skip+	 			// yes, exit
-			jsr DownOneRow 			// no, go one row down
-			ldy #$00 				// reset row character counter
-			jmp !loop-	 			// do next row
+	inc currentRow 			// go to the next row
+	lda currentRow 			// what value is it now?
+	cmp #20 				// all 20 rows checked?
+	beq !skip+	 			// yes, exit
+	jsr DownOneRow 			// no, go one row down
+	ldy #$00 				// reset row character counter
+	jmp !loop-	 			// do next row
 !skip:
-			rts
+	rts
 
 // ---------------------------------------------
 
 // restores the play area by reading the buffer
-
 RestorePlayArea:
-			ldx #12
-			ldy #0
-			jsr SetScreenPointer
+	ldx #12
+	ldy #0
+	jsr SetScreenPointer
 
-			ldx #$00 				// reset buffer counter
-			stx currentRow 			// and row index
+	ldx #$00 				// reset buffer counter
+	stx currentRow 			// and row index
 !loop:
-			lda playAreaBuffer,x 	// get buffer data
-			sta (screenPointer),y 	// store on screen
+	lda playAreaBuffer,x 	// get buffer data
+	sta (screenPointer),y 	// store on screen
 !skip:
-			inx 					// update buffer pointer
-			iny  					// update row character counter
-			cpy #10 				// done whole line?
-			bne !loop- 				// no
+	inx 					// update buffer pointer
+	iny  					// update row character counter
+	cpy #10 				// done whole line?
+	bne !loop- 				// no
 
-			inc currentRow 			// go to the next row
-			lda currentRow 			// what value is it now?
-			cmp #20 				// all 20 rows checked?
-			beq !skip+	 			// yes, exit
-			jsr DownOneRow 			// no, go one row down
-			ldy #$00 				// reset row character counter
-			jmp !loop-	 			// do next row
+	inc currentRow 			// go to the next row
+	lda currentRow 			// what value is it now?
+	cmp #20 				// all 20 rows checked?
+	beq !skip+	 			// yes, exit
+	jsr DownOneRow 			// no, go one row down
+	ldy #$00 				// reset row character counter
+	jmp !loop-	 			// do next row
 !skip:
-			rts
+	rts
 
 // ------------------------------------------------
 
-// this routine will copy data to another memory location
-// in this case the screen memory
-// source and dimensions must be set before calling this
+// screens mapped to their memory positions
+screenDataLow:
+	.byte <titleScreenData, <creditsScreenData, <keysScreenData
+	.byte <selectScreenData, <playscreen
+screenDataHigh:
+	.byte >titleScreenData, >creditsScreenData, >keysScreenData
+	.byte >selectScreenData, >playscreen
 
-WriteScreenData:
-			// get the source address
+// well text mapped to memory locations
+wellDataLow:
+	.byte <pauseText, <gameoverText
+wellDataHigh:
+	.byte >pauseText, >gameoverText
 
-			lda dataSourceLo
-			sta read+1
-			lda dataSourceHi
-			sta read+2
-
-			// get the destination address
-
-			lda dataDestinationLo
-			sta write+1
-			lda dataDestinationHi
-			sta write+2
-
-			ldx #$00 				// reset read index
-			ldy #$00 				// reset write index
-
-			// start copy
-read:
-			lda $1000,x 			// get data
-write:
-			sta $1000,y 			// store at destination
-			inx 					// update read counter
-			bne !skip+ 				// roll over?
-			inc read+2  			// yes. go to next memory page
-!skip:
-			iny 					// update row counter
-			cpy dataWidth			// this row done?
-			bne read 	 			// no, continue
-			ldy #$00 				// reset the row counter
-			lda write+1 			// get lo byte of current screen position
-			clc
-			adc #40 				// add 40 to that, goto next row
-			bcc !skip+ 				// overflow?
-			inc write+2  			// then go to next memory page
-!skip:
-			sta write+1 			// store lo byte
-
-			dec dataHeight 			// update counter
-			lda dataHeight
-			bne read 				// not all rows done yet
-			rts
-
-// where is the data coming from?
+// where is the screen data coming from?
 dataSourceHi:
 			.byte 0
 dataSourceLo:
 			.byte 0
 
 // what is the data size?
+// also used as counters!
 dataWidth:
 			.byte 0
 dataHeight:
@@ -179,4 +298,4 @@ playAreaErase:
 		.byte 0
 
 playAreaBuffer:
-		.fill 10*20, 0
+		.fill wellWidth * wellHeight, 0
